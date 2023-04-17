1c5eb4e4610dbc2e1bb6cb75cfe52fd7745167db
Omveer Singh <omveersingh9821@gmail.com>
2023-04-18 01:20:10 +0530

    Initial commit from Create Next App

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import styles from '@/styles/Home.module.css'
+
+
+
+export default function Home() {
+  return (
+    <>
+      <div className="App">
+        <h1>Home</h1>
+     </div>
+    </>
+  )
+}
